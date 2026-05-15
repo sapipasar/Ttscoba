37f71ca7ca4aff4154d38b9599f395ff9402385a
sapipasar <sapipasar97@gmail.com>
2026-05-15 17:29:32 +0700

    Change cache name and improve fetch handling
    Updated cache name and modified fetch strategy to prioritize cache.

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'video-editor-v1';
+const CACHE_NAME = 'v-editor-v1';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
   './icon_512.png'
 ];
 
-// Install Service Worker
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -14,22 +14,10 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Aktivasi & Pembersihan Cache Lama
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
-  );
-});
-
-// Strategi Fetch (Network First untuk fitur dinamis seperti TTS)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
